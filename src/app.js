f42a288222559993cfa933ae952395b44a73d433
LinoVives16 <linovivescolon@gmail.com>
2026-04-13 11:45:29 +0200

    Slack
    Implementación de slack

--- src/app.js
+++ src/app.js
@@ -4,10 +4,19 @@ import router from './routes/user.routes.js';
 import mongoSanitizeMiddleware from './middleware/sanitize.middleware.js';
 import limiter from './middleware/rate-limiter.middleware.js';
 import errorHandler from './middleware/errorHandler.middleware.js';
+import morganBody from 'morgan-body';
+import { loggerStream } from './utils/handleLogger.js';
 
 const app = express();
 
 app.use(express.json());
+
+morganBody(app, {
+  noColors: true,
+  skip: (req, res) => res.statusCode < 400, 
+  stream: loggerStream
+});
+
 app.use(helmet());
 app.use(mongoSanitizeMiddleware);
 
