1e728c69976fe9ed57d111fe4bd281041adb1205
LinoVives16 <linovivescolon@gmail.com>
2026-05-03 11:16:53 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
+import { createServer } from 'http';
 import app from './app.js';
 import mongoose from 'mongoose';
 import dbConnect from './config/db.js';
+import { initSocket } from './config/socket.js';
 
 dbConnect();
 
 const PORT = process.env.PORT || 3000;
 
-const server = app.listen(PORT, () => {
+const httpServer = createServer(app);
+const io = initSocket(httpServer);
+
+httpServer.listen(PORT, () => {
   console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
 
 const shutdown = async (signal) => {
   console.log(`${signal} recibido. Cerrando servidor...`);
-  server.close(async () => {
+  io.close();
+  httpServer.close(async () => {
     await mongoose.disconnect();
     console.log('Conexiones cerradas. Proceso terminado.');
     process.exit(0);
